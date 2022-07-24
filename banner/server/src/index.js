eb8e29884acc3080455b97020bce356e814c7b42
William <williamwijaya.mail@gmail.com>
2022-07-23 18:43:07 -0700

    Installing  express

--- banner/server/src/index.js
+++ banner/server/src/index.js
@@ -2,8 +2,8 @@ const OpenWeatherAPI = require("openweather-api-node")
 
 let weather = new OpenWeatherAPI({
     key: "fb4a842bfe7a9cc2b823b73ae6de7160",
-    locationName: "New York",
-    units: "imperial"
+    locationName: "Vancouver",
+    units: "metric"
 })
 
 /* 
@@ -14,6 +14,11 @@ weather.setLocationByName("New York")
 */
 
 weather.getCurrent().then(data => {
-    console.log(`Current temperature in New York is: ${data.weather.temp.cur}\u00B0F`)
+    console.log(`Current temperature in Vancouver is: ${data.weather.temp.cur}\u00B0C`)
     console.log("data", data)
 })
+
+
+// http://api.openweathermap.org/geo/1.0/zip?zip=V5R4W3,ca&appid=fb4a842bfe7a9cc2b823b73ae6de7160
+
+// https://api.openweathermap.org/data/2.5/weather?lat=49.2357&lon=-123.034&appid=fb4a842bfe7a9cc2b823b73ae6de7160
